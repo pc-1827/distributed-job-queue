0af8d464133a19138688464e1f66ba7b8c1ba921
pc-1827 <problemcreator1827@gmail.com>
2024-07-05 22:28:31 +0530

    Job failure done callback

--- main.js
+++ main.js
@@ -5,24 +5,35 @@ dotenv.config();
 
 const { REDIS_HOST, REDIS_PORT, REDIS_PASSWORD } = process.env;
 const redisOptions = {
-    redis: { host: REDIS_HOST, port: REDIS_PORT},
+    redis: { host: REDIS_HOST, port: REDIS_PORT, password: REDIS_PASSWORD },
 };
 
 const burgerQueue = new Queue("burger", redisOptions);
 
-const jobs = [...new Array(10)].map((_) => ({
-    bun: "🍔",
-    cheese: "🧀",
-    toppings: ["🍅", "🫒", "🥒", "🌶️"],
-}));
+(async () => {
+    const jobs = [...new Array(10)].map(() => ({
+        bun: "🍔",
+        cheese: "🧀",
+        toppings: ["🍅", "🫒", "🥒", "🌶️"],
+    }));
 
-jobs.forEach((job) => burgerQueue.addJobs(job, { attempt: 3, repeat: { cron: "10 * * * * *" }}));
+    for (const job of jobs) {
+        await burgerQueue.addJobs(job, { attempt: 3, repeat: { cron: "10 * * * * *" } });
+    }
 
-
-burgerQueue.processJobs((job, done) => {
-    console.log("Preparing the burger!");
-    setTimeout(() => {
-        console.log("Burger is ready!");
-        done();
-    }, 4000);
-});
+    burgerQueue.processJobs((job, done) => {
+        console.log("Preparing the burger!");
+        console.log(job);
+        setTimeout(() => {
+            try {
+                if (Math.random() < 0.5) {
+                    throw new Error("Failed to prepare the burger!");
+                }
+                console.log("Burger is ready!");
+                done();
+            } catch (error) {
+                done(error);
+            }
+        }, 4000);
+    });
+})();
